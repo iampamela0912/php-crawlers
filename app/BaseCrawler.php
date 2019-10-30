382dffca59ea2ce1c7bd66ced70a5cf0c0b17f60
pamela <pamela@HUAWEI_P10.localdomain>
2019-10-30 14:25:43 +0700

    use storage to put file

--- app/BaseCrawler.php
+++ app/BaseCrawler.php
@@ -1,18 +1,14 @@
 <?php
 
 namespace App;
+use Storage;
 
 class BaseCrawler
 {
-    const PATH = "storage/app/";
     const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36';
 
     public static function save($file_name, $file_content)
     {
-        $full_path = self::PATH.$file_name;
-        $myfile = fopen($full_path, "w");
-        fwrite($myfile, $file_content);
-        fclose($myfile);
-        return $full_path;
+        Storage::disk('local')->put($file_name,$file_content);
     }
 }
